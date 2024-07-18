4e0fc17b9445902b6a272921d04de4b82a0eaf8a
ArunkumarRScenai <151014607+ArunkumarRScenai@users.noreply.github.com>
2024-07-18 15:59:03 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
         git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('ArunkumarRScenai')
+            node / gitConfigEmail('arunkumar.rajan@scenai.com')
         }
     }
     triggers {
